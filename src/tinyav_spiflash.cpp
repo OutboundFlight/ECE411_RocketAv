265114b4239be283bc27ad35e0dc3f1479d41b2a
OutboundFlight <bitmapverse@gmail.com>
2016-12-06 18:50:15 -0800

    Comments and improvements to spiflash lib

--- src/tinyav_spiflash.cpp
+++ src/tinyav_spiflash.cpp
@@ -1,6 +1,19 @@
-// the sensor communicates using SPI, so include the library:
 #include <tinyav_spiflash.h>
+#define PAGE_TO_ADDRESS(a) (a << 8)
+#define ADDRESS_TO_PAGE(a) (a >> 8)
+#define ADDRESS_BYTE0(a) ((a & 0x110000) >> 16)
+#define ADDRESS_BYTE1(a) ((a & 0x001100) >> 8)
+#define ADDRESS_BYTE2(a) (a & 0x000011)
 
+//Pass a single-byte command to the device
+void spiflash::bytecommand(uint8_t code)
+{
+    digitalWrite(_cs, LOW);
+    SPI.transfer(code);
+    digitalWrite(_cs, HIGH);
+}
+
+//Initialize the device by setting up the select pin
 void spiflash::init(int cs)
 {
 	spiflash::_cs = cs;
@@ -8,80 +21,80 @@ void spiflash::init(int cs)
 	digitalWrite(_cs, HIGH);
 }
 
-void spiflash::writeEnable()
-{
-  digitalWrite(FLASH_SS, LOW);
-  SPI.transfer(FLASH_WEN);
-  digitalWrite(FLASH_SS, HIGH);
-}
+//Enable writing/block erase/chip erase
+void spiflash::writeEnable() 
+{ bytecommand(FLASH_WEN); }
 
+//disable writing/block erase/chip erase
 void spiflash::writeDisable()
-{
-	digitalWrite(FLASH_SS, LOW);
-	SPI.transfer(FLASH_WDIS);
-	digitalWrite(FLASH_SS, HIGH);
-}
+{ bytecommand(FLASH_WDIS); }
 
+//Erase chip by writing 1's to all memory locations
 void spiflash::chipErase()//
 {
-	writeEnable();
-	SPI.beginTransaction(SPISettings(SPICLOCK, MSBFIRST, SPI_MODE0));
-  digitalWrite(FLASH_SS, LOW);
-  SPI.transfer(FLASH_CHIPERASE);
-  digitalWrite(FLASH_SS, HIGH);
-	SPI.endTransaction();
-  for (int i = 0; i < NUM_PAGES; i++)
+	writeEnable(); //the flash chip wants a write enable command before each write
+
+	bytecommand(FLASH_CHIPERASE);
+
+    for (int i = 0; i < NUM_PAGES; i++)     //reset the page table
 	  page_table[i] = 0;
   
-  head = 0;
+  head = 0;                                 //set the head to lowest address
 
-  while(isBusy());  
-  writeEnable();
+  while(isBusy());                  //wait until page write completes
+  writeEnable();                    //another write enable for sanity
 }
 
+//reads the status register to determine if the chip is busy
 int spiflash::isBusy()
 {
-  digitalWrite(FLASH_SS, LOW);
-  SPI.transfer(FLASH_RDSR);
-  int sreg = SPI.transfer(0x00);
-  digitalWrite(FLASH_SS, HIGH);
-  return (sreg & FLASH_SR_WIP);
-
+    //retrieve status reg, binary and with bitmask for Write In Progress
+    return (readStatusRegister() & FLASH_SR_WIP);  
 }
 
+//read status register
 uint8_t spiflash::readStatusRegister()
 {
-	digitalWrite(FLASH_SS, LOW);
+	digitalWrite(_ce, LOW);
 	SPI.transfer(FLASH_RDSR);
 	int sreg = SPI.transfer(0x00);
-	digitalWrite(FLASH_SS, HIGH);
+	digitalWrite(_ce, HIGH);
 	return sreg;
 }
 
+//write the status register to unprotect the sectors.
+//NOT TESTED. DO NOT USE.
+/*
 void spiflash::unprotect()
 {
-  digitalWrite(FLASH_SS, LOW);
+  digitalWrite(_ce, LOW);
   SPI.transfer(FLASH_WRSR);
   SPI.transfer(0x00);
-  digitalWrite(FLASH_SS, HIGH);
+  digitalWrite(_ce, HIGH);
 }
 
+*/
+
+//Writes to the next available page in program memory
 int spiflash::writeSequentialPage(uint8_t *data, int numbytes)
 {
+    if (numbytes > PAGE_SIZE)
+        return -1;
+
 	while(page_table[head])
 		head++;
 	
-	if (!(head < NUM_PAGES))
+	if (head >= NUM_PAGES)
 		return -1;
 	
-	long addr = head << 8;
+	long addr = PAGE_TO_ADDRESS(head);
 	writeBytes(addr, data, numbytes);
 }
 
 void spiflash::writeBytes(long address, uint8_t *data, int numbytes)
 {
   writeEnable();
-  digitalWrite(FLASH_SS, LOW);
+  digitalWrite(_ce, LOW);
   SPI.transfer(FLASH_PP);
   SPI.transfer(address >> 16);
   SPI.transfer(address >> 8);
@@ -91,18 +104,51 @@ void spiflash::writeBytes(long address, uint8_t *data, int numbytes)
      SPI.transfer(*data);
      data++;
   }
-  digitalWrite(FLASH_SS, HIGH);
+  digitalWrite(_ce, HIGH);
   page_table[address >> 8] = 1;
   while(isBusy());
 }
 
+int nextWrittenPage(int readhead)
+{
+    do
+    {
+        readhead++;
+        if (readhead > NUM_PAGES)
+        {
+            readhead = -1;
+            break;
+        }
+    }while (page_table[readhead] == 0);
+    
+    return readhead;
+}
+
+void spiflash::readPage(int page, uint8_t *data)
+{
+    digitalWrite(_ce, LOW);
+
+    long addr = PAGE_TO_ADDRESS(page);
+
+    SPI.transfer(FLASH_FASTREAD);
+    SPI.transfer(ADDRESS_BYTE0(addr));
+    SPI.transfer(ADDRESS_BYTE1(addr));
+    SPI.transfer(ADDRESS_BYTE2(addr));
+    SPI.transfer(0x0);
+    for (int i = 0; i < PAGE_SIZE; i++)
+    {
+        *data = SPI.transfer(0x0);
+        data++
+    }
+}
+
 void spiflash::readBytes(long address, uint8_t *data, int numbytes)
 {
-  digitalWrite(FLASH_SS, LOW);
+  digitalWrite(_ce, LOW);
   SPI.transfer(FLASH_FASTREAD);
-  SPI.transfer(address >> 16);
-  SPI.transfer(address >> 8);
-  SPI.transfer(address);              //send the address as three bytes
+  SPI.transfer(ADDRESS_BYTE0(address));
+  SPI.transfer(ADDRESS_BYTE1(address));
+  SPI.transfer(ADDRESS_BYTE2(address));              //send the address as three bytes
   SPI.transfer(0x00);                 //dummy byte (required by chip)
   for (int i = 0; i < numbytes; i++)
   {
@@ -110,5 +156,5 @@ void spiflash::readBytes(long address, uint8_t *data, int numbytes)
     data++;
   }
  
-  digitalWrite(FLASH_SS, HIGH);
+  digitalWrite(_ce, HIGH);
 }
--- src/tinyav_spiflash.h
+++ src/tinyav_spiflash.h
@@ -1,9 +1,22 @@
+//A custom Arduino SPI library for the M25P10-A flash chip.
+//The chip can be initialized as an object by creating an object and calling
+//the spiflash::init(int _cs) function and passing _cs as a parameter.
+//
+//Note that this library does not do initialization of the SPI bus as a whole.
+//
+//Once initialized, the chip can be written to using the class methods. The
+//class also has a small amount of memory management for tracking which pages
+//have been written and writing to the next available unwritten page.
+
+
 #ifndef _SPIFLASH_H_
 #define _SPIFLASH_H_
 
 
 #include <SPI.h>
 
+
+//chip-specific flash command codes
 #define FLASH_RDSR 0x05
 #define FLASH_WRSR  0x02
 #define FLASH_WEN 0x06
@@ -13,16 +26,30 @@
 #define FLASH_CHIPERASE 0xC7
 #define FLASH_WDIS 0x04
 
-#define XM_SS 21
-#define GYR_SS 20
-#define FLASH_SS 22
-
-#define SPICLOCK 1000000
+#define FLASH_SR_WIP 0x01
 
 #define NUM_PAGES 512
 #define PAGE_SIZE 256
 
-#define FLASH_SR_WIP 0x01
+//implementation-specific select lines for SPI devices for the tinyav system
+#ifndef XM_SS
+#define XM_SS 21
+#endif
+
+#ifndef GYR_SS
+#define GYR_SS 20
+#endif
+
+#ifndef FLASH_SS
+#define FLASH_SS 22
+#endif
+
+//
+#ifndef SPICLOCK
+#define SPICLOCK 2000000
+#endif
+
+
 
 class spiflash
 {
@@ -39,9 +66,9 @@ class spiflash
 		int writeSequentialPage(uint8_t *data, int numbytes);
 		
 	protected:
-		uint8_t head;
-		uint8_t _cs;
-		bool page_table[NUM_PAGES];
+		uint8_t head;               //pointer to current memory location
+		uint8_t _cs;                //select line for this device
+		bool page_table[NUM_PAGES]; //tracks which pages have been written
 };
 
 #endif
